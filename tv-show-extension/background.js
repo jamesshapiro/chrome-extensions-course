dfb8e1c2c8649742df62eddba308249b6e578224
James Shapiro <jamesshapiro@gmail.com>
2021-05-25 21:27:34 -0700

    add tv show extension project

---
+++ tv-show-extension/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    title: "Test Context Menu",
+    id: "contextMenu1",
+    contexts: ["page", "selection"],
+  });
+  chrome.contextMenus.onClicked.addListener((event) => {
+    console.log(event);
+    chrome.tabs.create({
+      url: `https://www.imdb.com/find?q=${event.selectionText}&ref_=nv_sr_sm`,
+    });
+  });
+});
+
+console.log("background script running");
