978215ff6637050d0e5cf94eb786e80f0a8db411
James Shapiro <jamesshapiro@gmail.com>
2021-05-26 18:45:58 -0700

    finish TV show fetch API example

--- tv-show-extension/background.js
+++ tv-show-extension/background.js
@@ -1,15 +1,30 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({
+    shows: [],
+  });
   chrome.contextMenus.create({
     title: "Test Context Menu",
     id: "contextMenu1",
     contexts: ["page", "selection"],
   });
-  chrome.contextMenus.onClicked.addListener((event) => {
-    console.log(event);
-    chrome.tabs.create({
-      url: `https://www.imdb.com/find?q=${event.selectionText}&ref_=nv_sr_sm`,
-    });
+  chrome.contextMenus.create({
+    title: "Read This Text",
+    id: "contextMenu2",
+    contexts: ["page", "selection"],
   });
 });
 
-console.log("background script running");
+chrome.contextMenus.onClicked.addListener((event) => {
+  if (event.menuItemId === "contextMenu1") {
+    fetch(`http://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log();
+        chrome.storage.local.set({
+          shows: data,
+        });
+      });
+  } else if (event.menuItemId === "contextMenu2") {
+    chrome.tts.speak(event.selectionText);
+  }
+});
--- tv-show-extension/contentScript.js
+++ tv-show-extension/contentScript.js
@@ -1,1 +1,22 @@
+const text = [];
+
 console.log("Hello from the content script!");
+
+const aTags = document.getElementsByTagName("a");
+
+for (const tag of aTags) {
+  text.push(tag.textContent);
+}
+
+chrome.storage.local.set({
+  text,
+});
+
+chrome.runtime.sendMessage(null, text, (response) => {
+  console.log("I'm from the send response function! " + response);
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message);
+  console.log(sender);
+});
