41c1a070eca73f9a1aaf11fbe73c30a50e4b4c5b
James Shapiro <jamesshapiro@gmail.com>
2021-05-29 15:58:59 -0700

    add weather extension

---
+++ weather-extension/src/components/WeatherCard/WeatherCard.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Card,
+  CardActions,
+  Box,
+  Button,
+  CardContent,
+  Typography,
+} from '@material-ui/core'
+import {
+  fetchOpenWeatherData,
+  OpenWeatherData,
+  OpenWeatherTempScale,
+} from '../../utils/api'
+import './WeatherCard.css'
+
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode
+  onDelete?: () => void
+}> = ({ children, onDelete }) => {
+  return (
+    <Box mx={'4px'} my={'16px'}>
+      <Card>
+        <CardContent>{children}</CardContent>
+        <CardActions>
+          {onDelete && (
+            <Button color="secondary" onClick={onDelete}>
+              <Typography className="weatherCard-body">Delete</Typography>
+            </Button>
+          )}
+        </CardActions>
+      </Card>
+    </Box>
+  )
+}
+
+type WeatherCardState = 'loading' | 'error' | 'ready'
+
+const WeatherCard: React.FC<{
+  city: string
+  tempScale: OpenWeatherTempScale
+  onDelete?: () => void
+}> = ({ city, tempScale, onDelete }) => {
+  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null)
+  const [cardState, setCardState] = useState<WeatherCardState>('loading')
+
+  useEffect(() => {
+    fetchOpenWeatherData(city, tempScale)
+      .then((data) => {
+        setWeatherData(data)
+        setCardState('ready')
+      })
+      .catch((err) => setCardState('error'))
+  }, [city, tempScale])
+
+  if (cardState == 'loading' || cardState == 'error') {
+    return (
+      <WeatherCardContainer onDelete={onDelete}>
+        <Typography className="weatherCard-title">{city}</Typography>
+        <Typography className="weatherCard-body">
+          {cardState == 'loading'
+            ? 'Loading...'
+            : `Error: could not retrieve weather data for city: ${city}`}
+        </Typography>
+      </WeatherCardContainer>
+    )
+  }
+
+  return (
+    <WeatherCardContainer onDelete={onDelete}>
+      <Typography className="weatherCard-title">{weatherData.name}</Typography>
+      <Typography className="weatherCard-body">
+        {Math.round(weatherData.main.temp)}
+      </Typography>
+      <Typography variant="body1">
+        Feels like: {Math.round(weatherData.main.feels_like)}
+      </Typography>
+    </WeatherCardContainer>
+  )
+}
+
+export default WeatherCard
---
+++ weather-extension/src/utils/api.ts
@@ -0,0 +1,41 @@
+const OPEN_WEATHER_API_KEY = 'b923754cdad4c0a0f57da6230b865f76'
+
+export interface OpenWeatherData {
+  name: string
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+  }
+}
+
+export type OpenWeatherTempScale = 'metric' | 'imperial'
+
+export async function fetchOpenWeatherData(
+  city: string,
+  tempScale: OpenWeatherTempScale
+): Promise<OpenWeatherData> {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${tempScale}&appid=${OPEN_WEATHER_API_KEY}`
+  )
+
+  if (!res.ok) {
+    throw new Error('City not found')
+  }
+
+  const data: OpenWeatherData = await res.json()
+  return data
+}
